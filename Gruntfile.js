f21fbcee3c7b08f40347a1dd5b9bd8d362cdede2
Alejandro U. Alvarez <aluralma@gmail.com>
2015-06-03 13:38:31 +0100

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/sonar.css' : 'sass/sonar.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'sass/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
